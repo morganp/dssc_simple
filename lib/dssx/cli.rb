76e855a0d06db145ec0a17377fb887c9e282c11c
Morgan Prior <morgan@amaras-tech.co.uk>
2014-04-01 21:43:46 +0100

    Commands now accept multiple file inputs * add * remove (rm) * update (up) * commit (ci) * lock * revert

--- lib/dssx/cli.rb
+++ lib/dssx/cli.rb
@@ -7,20 +7,22 @@ module Dssx
   class CLI < Thor
     #include Thor::Actions
     desc "add [FILES]", "Add and Commit new files"
-    def add(files)
-      puts "dssx add #{files}" 
-      puts "=> dssc ci -new #{files}" 
+    def add(*files)
+      file_list = files * ' '
+      puts "dssx add #{file_list}" 
+      puts "=> dssc ci -new #{file_list}" 
     end
 
 
     desc "remove [PATH]", "Remove file/folder from repository"
-    def remove(path=Dir.pwd)
-      puts "dssx rm #{path}" 
-      puts "=> dssc retire #{path}" 
+    def remove(*path=Dir.pwd)
+      path_list = path * ' '
+      puts "dssx rm #{path_list}" 
+      puts "=> dssc retire #{path_list}" 
     end
 
     desc "rm [PATH]", "Alias for remove"
-    def rm(path=Dir.pwd)
+    def rm(*path=Dir.pwd)
       invoke :remove
     end
     
@@ -37,15 +39,16 @@ module Dssx
 
     option :r
     desc "update [PATH]", "Update to latest repository version"
-    def update(path=Dir.pwd)
-      puts "dssx update #{path}" 
-      cmd =  "dssc populate -recursive -replace -unify #{path}" 
+    def update(*path=Dir.pwd)
+      path_list = path * ' '
+      puts "dssx update #{path_list}" 
+      cmd =  "dssc populate -recursive -replace -unify #{path_list}" 
       cmd = cmd + " --version #{options[:r]}" if options[:r]
       puts "=> #{cmd}" 
     end
 
     desc "up [PATH]", "Alias for update"
-    def up(path=Dir.pwd)
+    def up(*path=Dir.pwd)
       invokee :update
     end
 
@@ -55,20 +58,22 @@ module Dssx
     end
 
     desc "commit[PATH]", "Commit changes to repository"
-    def commit(path=Dir.pwd)
-      puts "dssx commit #{path}" 
-      puts "=> dssc ci #{path}" 
+    def commit(*path=Dir.pwd)
+      path_list = path * ' '
+      puts "dssx commit #{path_list}" 
+      puts "=> dssc ci #{path_list}" 
     end
 
     desc "ci [PATH]", "Alias for commit"
-    def ci(path=Dir.pwd) 
+    def ci(*path=Dir.pwd) 
       invoke :commit
     end
 
     desc "lock [PATH]", "Lock file"
-    def lock(path=Dir.pwd) 
-      puts "dssx lock #{path}" 
-      puts "=> dssc co -lock -nocomment #{path}" 
+    def lock(*path=Dir.pwd) 
+      path_list = path * ' '
+      puts "dssx lock #{path_list}" 
+      puts "=> dssc co -lock -nocomment #{path_list}" 
     end
 
 
@@ -95,9 +100,10 @@ module Dssx
 
 
     desc "revert [PATH]", "Revert file back to previous version"
-    def revert(path=Dir.pwd)
-      puts "dssx revert #{path}" 
-      puts "=> dssc cancel -force #{path}" 
+    def revert(*path=Dir.pwd)
+      path_list = path * ' '
+      puts "dssx revert #{path_list}" 
+      puts "=> dssc cancel -force #{path_list}" 
     end
 
   end
